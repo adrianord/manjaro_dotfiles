9a61cc1c02e4ed7483d30fc0cbef13709eca1766
Adrian Ordonez <adrianord@gmail.com>
2021-10-15 13:09:43 -0500

    fix(zsh): alias cdcode and cddot pointed to correct directories

--- roles/zsh/files/config/.zsh.d/aliases.zsh
+++ roles/zsh/files/config/.zsh.d/aliases.zsh
@@ -1,7 +1,7 @@
 alias ls="ls --color=auto"
 alias la="ls -lAh"
-alias cdcode="cd ~/docs/code/"
-alias cddot="cd ~/.dotfiles"
+alias cdcode="cd ~/Code/"
+alias cddot="cd ~/.config/dotfiles"
 alias g=git
 alias bootstrap="${XDG_CONFIG_HOME:-${HOME}/.config/dotfiles/playbook/bin/bootstrap}"
 alias vim=nvim
